8aa739a26138f45428ed5a4e7823a784b14ab916
Kostiantyn_Ivanov <ICKostiantyn.Ivanov@amwell.com>
2021-12-30 16:52:31 +0200

    Add methods examples.

--- src/main/java/com/americanwell/demos/feignclient/CDRClient.java
+++ src/main/java/com/americanwell/demos/feignclient/CDRClient.java
@@ -4,6 +4,23 @@ import org.springframework.stereotype.Indexed;
 
 import java.lang.annotation.*;
 
+/*
+* Indicates an interface as CDR client.
+* All methods specified in this interface will be support by dynamic proxy
+*
+* Possible methods naming patterns:
+* {@code findOne[ResourceName]By[FieldName]And[FieldName]}: retrieves *one* object by input parameters.
+* Example: PractitionerRole findOnePractitionerRoleByPractitioner(String practitionerId)
+*
+* {@code findAll[ResourceName]By[FieldName]And[FieldName]}: retrieves *one* object by input parameters.
+* Example: List<MedicationRequest> findAllMedicationRequestsByEncounter(String encounterId)
+*
+* {@code save}: saves object from input parameter.
+* Example: Communication save(Communication communication)
+*
+* {@code update}: updates object from input parameter.
+* Example: Communication update(Communication communication)
+* */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
